60b917f41cd8a7b0a7e6b5eac69c0de2616e90a6
kyso8575 <kyso8575@gmail.com>
2025-01-21 19:39:06 +0900

    Edit, 챗봇, 데이터베이스 수정

--- games/models.py
+++ games/models.py
@@ -3,22 +3,6 @@ import json
 
 # Create your models here.
 
-# class Game(models.Model):
-#     name = models.CharField(max_length=255)
-#     released = models.DateField(null=True, blank=True)  # released는 NULL 허용
-#     background_image = models.URLField(null=True, blank=True)  # background_image는 NULL 허용
-#     rating = models.FloatField(default=0, null=True, blank=True)  # rating은 NULL 허용
-#     metacritic_score = models.IntegerField(default=0, null=True, blank=True)  # metacritic_score는 NULL 허용
-#     playtime = models.IntegerField(default=0, null=True, blank=True)  # playtime은 NULL 허용
-#     platforms = models.JSONField(null=True, blank=True)  # platforms는 NULL 허용
-#     genres = models.JSONField(null=True, blank=True)  # genres는 NULL 허용
-#     stores = models.CharField(max_length=255, null=True, blank=True)  # stores는 NULL 허용
-#     esrb_rating = models.CharField(max_length=255, null=True, blank=True)  # esrb_rating은 NULL 허용
-#     screenshots = models.JSONField(null=True, blank=True)  # screenshots는 NULL 허용
-
-#     def __str__(self):
-#         return self.name
-
 
 class Game(models.Model):
     name = models.CharField(max_length=255)
