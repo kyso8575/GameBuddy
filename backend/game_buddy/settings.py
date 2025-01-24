88d03b0232cb61f3b49706fdc2dcda08aea89900
kyso8575 <kyso8575@gmail.com>
2025-01-24 12:27:35 +0900

    Chore, Change directory structure

--- backend/game_buddy/settings.py
+++ backend/game_buddy/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'game_buddy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
